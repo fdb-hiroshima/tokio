986cae01b78107e00c6056aa3fb2ce443c87712a
Trinity Pointard <trinity.pointard@insa-rennes.fr>
2020-03-22 14:50:03 +0100

    fix PollEvented::poll_write_ready doc comment

--- tokio/src/io/poll_evented.rs
+++ tokio/src/io/poll_evented.rs
@@ -295,10 +295,7 @@ where
     ///
     /// # Panics
     ///
-    /// This function panics if:
-    ///
-    /// * `ready` contains bits besides `writable` and `hup`.
-    /// * called from outside of a task context.
+    /// This function will panic if called from outside of a task context.
     pub fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<mio::Ready>> {
         poll_ready!(
             self,
